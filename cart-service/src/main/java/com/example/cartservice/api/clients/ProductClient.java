b89debc569b9fe2289448dba1705493672ace376
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2024-01-24 18:11:48 +0300

    @Retry Annotation Enabled.

--- cart-service/src/main/java/com/example/cartservice/api/clients/ProductClient.java
+++ cart-service/src/main/java/com/example/cartservice/api/clients/ProductClient.java
@@ -1,6 +1,8 @@
 package com.example.cartservice.api.clients;
 
 import com.example.commonpackage.utils.dto.ClientResponse;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,5 +12,6 @@ import java.util.UUID;
 @FeignClient(name = "nvntry", fallback = ProductClientFallback.class)
 public interface ProductClient {
     @GetMapping(value = "/api/v1/products/check-quantity-exists/{productId}")
+    @Retry(name = "flightSearch")
     ClientResponse checkIfQuantityExists(@PathVariable UUID productId);
 }
